98da6b9024a7648297e602978c286eee9e3c4633
g4s-la <labworkspace@yandex.ru>
2021-02-12 17:30:08 +0300

    check

--- InfinityLoop/app/src/main/java/com/example/infinityloop/MainActivity.java
+++ InfinityLoop/app/src/main/java/com/example/infinityloop/MainActivity.java
@@ -18,51 +18,79 @@ public class MainActivity extends AppCompatActivity {
 
     private final Handler mHandler = new Handler(Looper.getMainLooper());
     private TextView tvNumber;
-    private boolean flag = true;
+    private SharedPreferences sPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyViewModel model = ViewModelProviders.of(this).get(MyViewModel.class);
-
         tvNumber = findViewById(R.id.tv_number);
-//        loadNumber();
-
-        tvNumber.setOnClickListener(v -> {
-            tvNumber.setText("10");
-        });
-
-        if(flag) {
-            flag = false;
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    tvNumber.setText(String.valueOf(viewToLongPlusOne(tvNumber)));
-                    Log.d("...", tvNumber.getText().toString());
-                    mHandler.postDelayed(this, 1000);
-                }
-            });
-        }
+        loadNumber();
+        mHandler.post(tikTak);
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putString("KEY", tvNumber.getText().toString());
-        outState.putBoolean("Bool", b);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        tvNumber.setText(savedInstanceState.getString("KEY","0"));
-        b = savedInstanceState.getBoolean("Bool", true);
-
-    }
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        outState.putString("KEY", tvNumber.getText().toString());
+//        super.onSaveInstanceState(outState);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        tvNumber.setText(savedInstanceState.getString("KEY"));
+//    }
 
     private long viewToLongPlusOne(TextView view){
         return Long.parseLong(view.getText().toString()) + 1;
+    }
+
+    private final Runnable tikTak = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("...", tvNumber.getText().toString());
+            mHandler.postDelayed(this, 1000);
+            tvNumber.setText(String.valueOf(viewToLongPlusOne(tvNumber)));
+        }
+    };
+
+    private void saveNumber(){
+        sPref = getPreferences(MODE_PRIVATE);
+        editor = sPref.edit();
+        editor.putLong("Time", System.currentTimeMillis());
+        editor.apply();
+    }
+
+    private void loadNumber(){
+        sPref = getPreferences(MODE_PRIVATE);
+        long pastTime = sPref.getLong("Time",System.currentTimeMillis());
+        tvNumber.setText(String.valueOf( Long.parseLong(tvNumber.getText().toString()) + System.currentTimeMillis()-pastTime));
+    }
+
+    @Override
+    public boolean deleteSharedPreferences(String name) {
+        return super.deleteSharedPreferences(name);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveNumber();
+        mHandler.removeCallbacks(tikTak);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mHandler.post(tikTak);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        editor.remove("Time");
+        editor.apply();
     }
 }
